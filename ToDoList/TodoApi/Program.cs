1602cf448586852239dcb47c1abcd1cb39a6892b
PabloPerfernandez <132550960+PabloPerfernandez@users.noreply.github.com>
2024-06-16 19:52:47 +0200

    16/6

--- ToDoList/TodoApi/Program.cs
+++ ToDoList/TodoApi/Program.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+/* using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi.Models;
 using TodoApi.Models;
@@ -7,6 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAllOrigins", builder =>
@@ -18,16 +26,19 @@ builder.Services.AddCors(options =>
 });
 
 var app = builder.Build();
+//comment
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
 }
 else
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseExceptionHandler("/Error");
     app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -43,5 +54,45 @@ app.MapControllers();
 
 // Redirigir todas las rutas al archivo index.html
 app.MapFallbackToFile("index.html");
+
+app.Run(); */
+
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseCors("AllowAllOrigins");
 
 app.Run();
---
+++ ToDoList/todo-list/src/components/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>About</h1>
+      <p>About this app.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'About-About'
+  };
+  </script>
